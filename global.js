dfb2d409b1f1f36992ab59cafaccd016b3164dad
MasterAli2 <seperoa2li@gmail.com>
2025-02-22 16:58:19 -0500

    refresh
    add meta data

--- global.js
+++ global.js
@@ -38,6 +38,14 @@ function OnPageLoaded()
     document.head.removeChild(oldLink);
     }
     document.head.appendChild(link);
+
+
+    document.head.innerHTML+=`
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Friendly Fish">
+    <meta property="og:description" content="Silly website I made\n Friendly Fish™">
+    <meta property="og:image" content="https://friendlyfish.dev/icon.png">
+    `
 }
 
 OnPageLoaded()
